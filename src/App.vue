5c9b1c5de21fdbba96f3772bba3bdb71b4bb66d9
yuKalasnikova2 <pmalian@list.ru>
2024-01-24 11:23:24 +0300

    initiall commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useCounterStore } from './store/index.js'
+
+const store = useCounterStore()
+</script>
+
+<template>
+  <h1>Pinia + Firebase Storage + Vue 3</h1>
+  <div>
+    {{ store.name }}
+  </div>
+
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import './style.css'
+import App from './App.vue'
+import { initializeApp } from "firebase/app";
+import { getStorage} from "firebase/storage"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDUr0CHYVfmpVxygonZUWyUHA_dQyF3zc4",
+    authDomain: "pinia-upload-files.firebaseapp.com",
+    projectId: "pinia-upload-files",
+    storageBucket: "pinia-upload-files.appspot.com",
+    messagingSenderId: "332387610269",
+    appId: "1:332387610269:web:314ea9a02f2858de8b6e74"
+  };
+
+let defaultProject = initializeApp(firebaseConfig);
+let defaultStorage = getStorage(defaultProject);
+
+const pinia = createPinia()
+createApp(App).use(pinia).mount('#app')
+
+
